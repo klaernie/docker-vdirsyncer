d823b6a6110568797d23d262700254a5af2b60de
Andre Klärner <kandre@ak-online.be>
2021-01-26 16:42:54 +0100

    pass arguments through the entrypoint, mostly to allow vdirsyncer -vdebug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/usr/local/bin/vdirsyncer discover || exit 1
+/usr/local/bin/vdirsyncer discover "$@" || exit 1
 until false
 do
-   /usr/local/bin/vdirsyncer sync
+   /usr/local/bin/vdirsyncer sync "$@"
    sleep ${VDIRSYNCER_INTERVAL:-900}
 done
