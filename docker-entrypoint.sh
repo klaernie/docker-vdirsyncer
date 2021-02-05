006af7693afb04be04a11d4c5cd0b6e102de36bd
Andre Klärner <kandre@ak-online.be>
2021-02-05 11:26:04 +0100

    entrypoint: replace static version with a config-overridable file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,7 +19,8 @@ until false
 do
    # shellcheck disable=SC2086
    /usr/local/bin/vdirsyncer $DEBUG sync "$@"
-   test -d /home/vds/calendars && chmod a+r /home/vds/calendars
+
+   test -x /home/vds/post-sync-hook.sh && bash /home/vds/post-sync-hook.sh
 
    sleep "${VDIRSYNCER_INTERVAL:-900}"
 done
